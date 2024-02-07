61e11d358ddb49a744665e853077c6f908a2591d
Matei Macoveiciuc-Hreamata <Matei.Macoveiciuc-Hreamata@msg.group>
2024-02-07 14:32:53 +0200

    tuturial pdf

--- sender/src/main/java/com/matei/soa/sender/rest/Controller.java
+++ sender/src/main/java/com/matei/soa/sender/rest/Controller.java
@@ -1,21 +1,24 @@
 package com.matei.soa.sender.rest;
 
-import com.matei.soa.sender.rabbitmq.Sender;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.matei.soa.sender.rabbitmq.Sender;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class Controller {
+
     private final Sender sender;
 
     @GetMapping
     public void receiveRestMessage(@RequestParam String message) {
-        log.info("received message {}", message);
+        log.info("received from rest endpoint: {}", message);
         sender.sendMessage(message);
     }
 }
